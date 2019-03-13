217ee63d311634d8ecbb71d685cc3b717d4729a4
bragliavanessa <braglv@usi.ch>
2019-03-13 16:43:11 +0100

    comments

--- Project/video_to_frame.py
+++ Project/video_to_frame.py
@@ -1,10 +1,13 @@
+# Take a video from the ./video folder and return the frames in the ./frames folder
+# https://stackoverflow.com/questions/33311153/python-extracting-and-saving-video-frames
+
 import cv2
 
 vidcap = cv2.VideoCapture('./video/prova.mp4')
 success,image = vidcap.read()
 count = 0
 while success:
-  cv2.imwrite("./frames/frame%d.jpg" % count, image)     # save frame as JPEG file      
+  cv2.imwrite("./frames/frame%d.jpg" % count, image)    # Save the frame 
   success,image = vidcap.read()
   print('Read a new frame: ', success)
   count += 1
