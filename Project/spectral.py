c2e8c7ca0282bd9275264611d36c722a097a894a
liudmilakaragyaur <karagl@usi.ch>
2019-03-31 16:47:02 +0200

    spectral adjustments

--- Project/spectral.py
+++ Project/spectral.py
@@ -9,7 +9,7 @@ from sklearn.cluster import spectral_clustering
 from sklearn.metrics import pairwise_distances
 np.set_printoptions(threshold=np.inf)
 
-img = cv2.imread('./frames/frame0.png')
+img = cv2.imread('./frames/frame102.png')
 img = cv2.resize(img, None, fx=0.1, fy=0.1, interpolation=cv2.INTER_CUBIC)
 # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -23,11 +23,21 @@ def image_to_graph(img):
             graph_rgb.append([r, g, b])
     return [np.array(graph_xy), np.array(graph_rgb)]
 
+def kNNSimGraph(D):
+    # k = int(np.ceil(2*np.log(len(D)))); 
+    k=150
+    M = np.zeros(D.shape)
+    for (index,row) in enumerate(D):
+        sort_idx = np.argsort(row)[:k]
+        M[index][sort_idx] = 1
+    return M
+
+
 
 [graph_xy, graph_rgb] = image_to_graph(img)
 n = len(graph_xy)
 sigma_xy = 2*np.log(n)
-sigma_rgb = 2*np.log(n)
+sigma_rgb = 2*np.log(n)*4
 
 dists_xy = sp.spatial.distance.pdist(graph_xy)
 dists_rgb = sp.spatial.distance.pdist(graph_rgb)
@@ -41,11 +51,13 @@ S_xy = np.exp(-(D_xy**2) / (2*(sigma_xy**2)))
 S_rgb = np.exp(-(D_rgb**2) / (2*(sigma_rgb**2)))
 
 
-S = S_xy+S_rgb
+S = S_xy + S_rgb
 
-epsilon = 150
+# epsilon = np.max(sp.sparse.csgraph.minimum_spanning_tree(S).toarray())
+epsilon = 100
 G = np.array([x < epsilon for x in D])
 G = G.astype(int)
+# G = kNNSimGraph(D) 
 
 W = G * S
 W = sp.sparse.csr_matrix(W)
@@ -53,8 +65,7 @@ W = sp.sparse.csr_matrix(W)
 K = 3
 
 
-labels = spectral_clustering(W, n_clusters=K,
-                             assign_labels='kmeans', random_state=42, eigen_solver='amg')
+labels = spectral_clustering(W, n_clusters=K, assign_labels='kmeans', random_state=42, eigen_solver='amg')
 labels = labels.reshape(img.shape[:2])
 
 plt.figure(figsize=(5, 5))
