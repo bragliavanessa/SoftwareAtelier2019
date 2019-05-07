962b289b6ce912df2cecc7306c550940b8be8975
bragliavanessa <braglv@usi.ch>
2019-05-07 16:51:58 +0200

    kmeans function

--- Project/kmeans.py
+++ Project/kmeans.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-img = cv2.imread('./frames/frame2.png')
+img = cv2.imread('./frames/a_masked.png')
 img = cv2.resize(img, None, fx=1, fy=1, interpolation=cv2.INTER_CUBIC)
 
 
@@ -15,22 +15,15 @@ def image_to_graph(img):
     return np.array(graph)
 
 
-Z = image_to_graph(img)
-Z = np.float32(Z)
+def kmeans(img,K):
+    Z = image_to_graph(img)
+    Z = np.float32(Z)
 
-# define criteria, number of clusters(K) and apply kmeans()
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 5
-ret, label, center = cv2.kmeans(
-    Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    # define criteria, number of clusters(K) and apply kmeans()
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    ret, label, center = cv2.kmeans(
+        Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
-# Now convert back into uint8, and make original image
-labels = label.reshape(img.shape[:2])
-plt.figure(figsize=(5, 5))
-plt.imshow(img)
-plt.imshow(labels*255/K, alpha=0.8)
+    labels = label.reshape(img.shape[:2])
+    return label
 
-for l in range(K):
-    plt.contour(labels == l,
-                colors=[plt.cm.nipy_spectral(l / float(K))])
-plt.show()
